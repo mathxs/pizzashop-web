c47af39321624f7f3d495a7df5646e79a7fc70ea
mathxs <matheusdsantosp@gmail.com>
2024-10-14 11:03:54 -0300

    ajuste na sign

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,11 +1,35 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 import { Helmet } from 'react-helmet-async'
 
 export function SignIn() {
   return (
-    <div>
+    <>
       <Helmet title="Login" />
-      <h1>Sign In</h1>
-      <p>This is the sign in page</p>
-    </div>
+      <div className="p-8">
+        <div className="w-[350px] flex flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acesso painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro
+            </p>
+          </div>
+
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" placeholder="Digite seu email" />
+            </div>
+
+            <Button className="w-full" type="submit">
+              Acessar Painel
+            </Button>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
