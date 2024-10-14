7d2fed101eb5c8d2a93a248dab51a7ba3d53ddf0
mathxs <matheusdsantosp@gmail.com>
2024-10-14 09:22:08 -0300

    botão basico

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Button } from './components/ui/button'
+import './global.css'
+
 export function App() {
 
   return (
-    <h1>
-      Hello, world!
-    </h1>
+    <div className='flex justify-center items-center h-screen'>
+      <Button>Click me</Button>
+    </div>
   )
 }
