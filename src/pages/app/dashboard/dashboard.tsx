4f08e39e475548960475a9197d75df1110935f02
mathxs <matheusdsantosp@gmail.com>
2024-10-16 09:38:28 -0300

    fim do modulo 2

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -4,6 +4,8 @@ import { DayOrdersAmountCard } from './day-orders-amount-card'
 import { MonthCanceledOrdersAmountCard } from './month-canceled-orders-amount-card'
 import { MonthOrdersAmountCard } from './month-orders-amount-card'
 import { MonthRevenueCard } from './month-revenue-card'
+import { PopularProductsChar } from './popilar-products-chart'
+import { RevenueChart } from './revenue-chart'
 export function Dashboard() {
   return (
     <>
@@ -15,6 +17,11 @@ export function Dashboard() {
           <MonthOrdersAmountCard />
           <DayOrdersAmountCard />
           <MonthCanceledOrdersAmountCard />
+        </div>
+
+        <div className="grid grid-cols-2 gap-4">
+          <RevenueChart />
+          <PopularProductsChar />
         </div>
       </div>
     </>
